7f1aa6e3bff31e1bfaa80ae3b8097a4933162ed4
Tom Marshall <tdm@cyngn.com>
2014-05-13 13:52:28 -0700

    apq8064: Fix baseband version
    The baseband version string is set by both RIL and this script.  But RIL is
    setting the empty string for some reason.  Both of these processes run
    independently and race with each other.  Fix the race by waiting up to 15
    seconds after boot complete for RIL to set the baseband version string,
    then overwrite it with the correct string.
    
    Change-Id: Ic9046c0a2c6b2bba1666fa1acb6fb370173fb59a

--- rootdir/etc/init.bbv.sh
+++ rootdir/etc/init.bbv.sh
@@ -4,5 +4,20 @@
 PATH=/sbin:/system/sbin:/system/bin:/system/xbin
 export PATH
 
-setprop gsm.version.baseband `strings /firmware/image/dsp2.mbn  | grep -B1 "^M9615" | head -1`
+if [ ! -f "/firmware/image/dsp2.mbn" ]; then
+	exit 0
+fi
+ver=`strings /firmware/image/dsp2.mbn  | grep -B1 "^M9615" | head -1`
+
+tries=15
+while [ "$tries" -gt 0 ]; do
+	tries=$((tries-1))
+	n=$(getprop | grep "gsm.version.baseband" | wc -l)
+	if [ "$n" -gt 0 ]; then
+		break
+	fi
+	sleep 1
+done
+
+setprop gsm.version.baseband "$ver"
 

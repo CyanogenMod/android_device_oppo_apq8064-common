4d3b88c7d0c76e36ecf0f3dc468215c1d6aae0f3
feelingfree-me <rattapoom_m@cmu.ac.th>
2015-07-28 19:18:51 +0700

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,6 +107,10 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom.std
 BOARD_SEPOLICY_DIRS += \
     device/oppo/apq8064-common/sepolicy
 
+BOARD_SEPOLICY_UNION += \
+    lvm.te \
+    init.te
+
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_WLAN_DEVICE := qcwcn
